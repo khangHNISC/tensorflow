f512ccf8ab04ddee9b4b38a2aa9d67ea6bf80f44
khangHNISC <59554204+khangHNISC@users.noreply.github.com>
2023-02-28 15:18:14 +0700

    just dont know where i left off

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -2,8 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('loss') < 0.4:
+            self.model.stop_training = True
+
+
 if __name__ == '__main__':
     print(tf.__version__)
+    callback = myCallback()
     fashion_mnist = tf.keras.datasets.fashion_mnist
     # 60000 vs 10000
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
@@ -14,20 +22,28 @@ if __name__ == '__main__':
     print(f'Label: {train_labels[index]}')
     print(f'Image pixel array: {train_images[index]}')
     plt.imshow(train_images[index], cmap='Greys')
-    plt.show()  # https://stackoverflow.com/questions/42812230/why-doesnt-plt-imshow-display-the-image
+    # plt.show()  # https://stackoverflow.com/questions/42812230/why-doesnt-plt-imshow-display-the-image
 
-    # normalize pixel
+    # normalize data
     train_images = train_images / 255.0
     test_images = test_images / 255.0
 
     # train
     model = tf.keras.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation=tf.nn.relu),
         tf.keras.layers.Dense(10, activation=tf.nn.softmax),
     ])
 
     model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit(train_images, train_labels, epochs=5)
+    model.fit(train_images, train_labels, epochs=5, callbacks=[callback])
+
+    # loss = 0.29xx = not great
 
     model.evaluate(test_images, test_labels)
+
+    # loss = 0.345
+    classifications = model.predict(test_images)
+
+    print(classifications[0])
+    print(test_labels[0])
--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,27 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == '__main__':
     print(tf.__version__)
-    model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-    model.compile(optimizer='sgd', loss='mean_squared_error')
+    model = tf.keras.Sequential([tf.keras.layers.Dense(1, input_shape=[1])])
+    print(model.output_shape)
+    optimizer = tf.keras.optimizers.SGD()
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
 
-    xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-    ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+    xs = np.array(list(range(0, 20)), dtype=float)
+    ys = np.array([2 * x - 1 for x in range(0, 20)], dtype=float)
+    x_scaler = StandardScaler()
+    y_scaler = StandardScaler()
 
-    model.fit(xs, ys, epochs=500)
-    print(model.predict([10.0]))
+    xs_new = x_scaler.fit_transform(np.array(xs).reshape(-1, 1))
+    ys_new = y_scaler.fit_transform(np.array(ys).reshape(-1, 1))
+
+    model.fit(xs_new, ys_new, epochs=500, verbose=1)
+    test_x = np.array([21]).reshape(-1, 1)
+    x1 = xs / 20
+    y1 = ys / 37
+    # model.fit(xs, ys, epochs=2000)
+    # print(y_scaler.inverse_transform(model.predict(x_scaler.transform(test_x))))
+
+    print(model.predict([21]))
